d5a961e12dc39d02cd7b18be4a4ea5bb8c5e7ccf
jdowie-ada <joshua@ada.ac.uk>
2025-02-18 12:29:35 +0000

    Add fetching a list of pokemon

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const test_url = 'https://pokeapi.co/api/v2/pokemon/1';
+const test_url = 'https://pokeapi.co/api/v2/pokemon?limit=5';
 document.getElementById('fetchPokemon').addEventListener('click', () => fetchData(test_url));
-
 
 async function fetchData(url) {
     try {
         const response = await fetch(url);
+
         const data = await response.json(); // Parse the response as JSON
-        console.log(data); // Handle the data
+        console.log("Here is the raw fetched data", data); // Handle the data
+
+        const pokeList = document.getElementById("poke-list");
+        const {results: fetchedPokemon} = data;
+
+        fetchedPokemon.forEach(element => {
+            pokeList.innerHTML += `<p>${element.name}</p>`;
+        });
     } catch (error) {
         console.error('Error:', error); // Handle any errors
     }
